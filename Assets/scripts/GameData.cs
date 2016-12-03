8aa52891745b371b471a5a8ae59b8c315bf6188b
seraphki <sephki@gmail.com>
2016-12-02 21:10:49 -0800

    Some Data Structure Setup

---
+++ Assets/scripts/GameData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameData
+{
+	public enum StarType { Star, Circle, Triangle, Square };
+
+	public class Star
+	{
+		public StarType Type;
+		public Vector2 Position;
+	}
+
+	public class StarkLink
+	{
+
+	}
+
+	public class Constellation
+	{
+
+	}
+}
