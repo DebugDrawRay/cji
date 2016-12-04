169ff256dfe27d449eb0236ed82bdda43dd06e30
Logan <fiethlogan@gmail.com>
2016-12-03 19:39:17 -0800

    got pinging all stars of the same type working again

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -92,10 +92,13 @@ public class PlayerController : MonoBehaviour
                 lastStar = isStar;
                 isStar.StopMovement();
                 //isStar.starBoing.GetComponent<StarBoingController>().StartGrowing();
-                isStar.starBoing.gameObject.SetActive(true);//active the star boing
+                //isStar.starBoing.gameObject.SetActive(true);//active the star boing
 
-                StarController isStarStarCont = isStar.GetComponent<StarController>();
-                isStarStarCont.delayBeforeSecondBoingTimer = isStarStarCont.delayBeforeSecondBoingTimerBase;//start the timer for the 2nd star boing
+               // StarController isStarStarCont = isStar.GetComponent<StarController>();
+                //isStarStarCont.delayBeforeSecondBoingTimer = isStarStarCont.delayBeforeSecondBoingTimerBase;//start the timer for the 2nd star boing
+                isStar.DoBoing();
+
+                FindAllStarsOfSameTypeAndBoingThem(isStar);
 
                 isStar.starData.Position = isStar.transform.position;
                 ChangeColor(isStar.theStarType);
@@ -143,6 +146,35 @@ public class PlayerController : MonoBehaviour
         {
             line.SetPosition(0, line.transform.position);
             line.SetPosition(1, lastStar.transform.position);
+        }
+    }
+
+    void FindAllStarsOfSameTypeAndBoingThem(StarController _isStar)
+    {
+        GameObject[] allStars = GameObject.FindGameObjectsWithTag("Star");
+
+        for (int i = 0; i < allStars.Length; i++)
+        {
+            if (_isStar.theStarType == allStars[i].GetComponent<StarController>().theStarType)//if the star that is passed is the same as the star that you come across in the array
+            {
+                //ping the star in the allStars array
+                if(allStars[i].activeSelf == true)//make sure it's active
+                {
+                    allStars[i].GetComponent<StarController>().DoBoing();//boing it
+                }
+            }
+            /*else if (_isStar.theStarType == GameData.StarType.Square)
+            {
+
+            }
+            else if (_isStar.theStarType == GameData.StarType.Triangle)
+            {
+
+            }
+            else if (_isStar.theStarType == GameData.StarType.Star)
+            {
+
+            }*/
         }
     }
 }
